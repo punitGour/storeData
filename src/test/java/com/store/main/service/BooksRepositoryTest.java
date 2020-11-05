5ce0b31cea5835c53985135ed4f6c2e9d19f11cf
Punit Gour <punitg@cybage.com>
2020-11-05 10:54:49 +0530

    added test case

---
+++ src/test/java/com/store/main/service/BooksRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.store.main.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.store.main.model.Books;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class BooksRepositoryTest {
+	
+	@Autowired
+	private BooksRepository repo;
+	
+	@Test
+	@Rollback(false)
+	public void testCreateBooks() {
+//	    Books books = repo.save(new Books("1", "linux", "20", "punit", "2000", "20", "new"));
+//	     
+//	    assertThat(books.getID()).isEqualTo("1");
+	}
+
+}
